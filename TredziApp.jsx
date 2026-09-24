c1477abe49587356145d592807e25130265a36fd
Tredzi <hkrraihan868@gmail.com>
2026-09-24 20:07:28 +0600

    Update TredziApp.jsx

--- TredziApp.jsx
+++ TredziApp.jsx
@@ -19420,7 +19420,7 @@ if (activeTab === "community") {
     // ---------- MOBILE GROUP ----------
     body = renderChatPanel({ height: "100%" });
   }
-}
+
   return (
     <div
       className="w-full flex justify-center"
